9498f139719a8ef4917e4beccdaf59c4a7085cd3
CookieKastanie <33513743+CookieKastanie@users.noreply.github.com>
2024-05-12 22:02:15 +0200

    ...

--- src/process/Clipboard.js
+++ src/process/Clipboard.js
@@ -1,16 +1,14 @@
 export class Clipboard {
     static copyText(text) {
-        navigator.clipboard.writeText(text).catch(() => {
-            if (!navigator.clipboard){
-                const element = document.createElement('input');
-                document.body.appendChild(element);
-                element.value = text;
-                element.select();
-                document.execCommand('copy');
-                document.body.removeChild(element);
-            } else {
-                navigator.clipboard.writeText(text);
-            }
-        });
+        if (navigator.clipboard){
+            navigator.clipboard.writeText(text).catch(console.error);
+        } else {
+            const element = document.createElement('input');
+            document.body.appendChild(element);
+            element.value = text;
+            element.select();
+            document.execCommand('copy');
+            document.body.removeChild(element);
+        }
     }
 }
